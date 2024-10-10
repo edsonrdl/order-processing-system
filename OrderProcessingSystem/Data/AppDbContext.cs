74def2ab5dcf463c15cf3484387bbb154f2ea871
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2024-10-10 00:26:47 -0300

    feito implementações

--- OrderProcessingSystem/Data/AppDbContext.cs
+++ OrderProcessingSystem/Data/AppDbContext.cs
@@ -1,6 +1,12 @@
-﻿namespace OrderProcessingSystem.Data
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OrderProcessingSystem.Data
 {
     public class AppDbContext:DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
     }
 }
--- OrderProcessingSystem/Program.cs
+++ OrderProcessingSystem/Program.cs
@@ -1,16 +1,37 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using OrderProcessingSystem.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+// Obtenha a string de conexão do PostgreSQL
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+                       ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+// Configura o AppDbContext para usar PostgreSQL
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql(connectionString);
+});
+
+
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
